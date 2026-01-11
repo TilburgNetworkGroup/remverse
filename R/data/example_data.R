d4657a9b4538ff35f307b2ffbf0a189779012962
jomulder <jomulder@gmail.com>
2026-01-11 14:45:00 +0100

    Stop tracking .DS_Store

---
+++ R/data/example_data.R
@@ -0,0 +1,21 @@
+
+
+#' Example dataset: tie_data
+#'
+#' Example relational event history dataset. Source: \code{remstimate}.
+#'
+#' @docType data
+#' @name tie_data
+#' @keywords datasets
+NULL
+
+
+#' Example dataset: ao_data
+#'
+#' Example relational event history dataset. Source: \code{remstimate}.
+#'
+#' @docType data
+#' @name ao_data
+#' @keywords datasets
+NULL
+
